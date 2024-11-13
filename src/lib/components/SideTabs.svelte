ab1d8e829024cf6c459df0b7c3fea8a7a25edf4b
radiorambo <thisisumesh@proton.me>
2024-11-13 22:18:03 +0530

    rename and refactor SideTabs component

---
+++ src/lib/components/SideTabs.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	interface Tab {
+		name: string;
+		routeId: string;
+	}
+
+	const { tabs }: { tabs: Tab[] } = $props();
+</script>
+
+{#each tabs as tab}
+	<a
+		href={tab.routeId}
+		class=" -mr-2 ml-16 px-20 py-12 text-left text-sm lg:text-base
+{$page.route.id?.includes(tab.routeId)
+			? '  bg-primary-light font-medium hover:bg-primary-light  dark:bg-primary-dark hover:dark:bg-primary-dark'
+			: 'font-normal hover:bg-primary-light/50 hover:dark:bg-primary-dark/10 '}"
+	>
+		{tab.name}
+	</a>
+{/each}
