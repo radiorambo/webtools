3b17241dac7f29195270214871b51ad3a7dc25f9
radiorambo <thisisumesh@proton.me>
2024-11-17 15:13:21 +0530

    add mdsvex package

--- src/app.d.ts
+++ src/app.d.ts
@@ -8,6 +8,12 @@ declare global {
 		// interface PageState {}
 		// interface Platform {}
 	}
+	// mdsvex
+	declare module '*.md' {
+		import type { SvelteComponent } from 'svelte';
+		export default class Comp extends SvelteComponent {}
+		export const metadata: Record<string, unknown>;
+	}
 }
 
 export {};
